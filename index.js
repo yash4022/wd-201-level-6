3deb382eadf01eafe2e45a015e1c3d651200516e
yash4022 <113338563+yash4022@users.noreply.github.com>
2023-01-07 18:17:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const app = require("./app");
-
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+const app = require('./app');
+app.listen(process.env.PORT || 3003, ()=>{
+  console.log('Server has been started');
 });
